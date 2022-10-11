ef86324c97c342c40cd7cdbdbd04b8a5f53ac940
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-10 20:19:21 -0400

    commit tree 6c81d67dd2139a7bacb021abe965ca077d4c5c05

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2;
+let num3 = 5;
+let num4 = 14;
 
-// Write your code here
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let mod = num4 % num3;
+
+let max = Math.max(2, 2,4, 2, 20);
